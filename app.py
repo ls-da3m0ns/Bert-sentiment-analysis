999215763e884d79457502cdb53389a52a1c86cc
prashant kumar singh <backbencherspot@gmail.com>
2020-09-08 23:54:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ if __name__ == "__main__":
     MODEL.load_state_dict(torch.load(config.MODEL_PATH, map_location=torch.device('cpu')))
     MODEL.to(DEVICE)
     MODEL.eval()
-    app.run(host="127.0.0.1", port="9999",debug=True)
+    app.run(host="0.0.0.0", port="9999",debug=True)
